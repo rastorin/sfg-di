63f40121e6d33b0b453aa2b12956b1312fd17da6
rastorin <rastorin@ENMAXVDGNPRP039.enmax.com>
2020-03-10 06:31:04 -0600

    not a good tutorial

--- src/main/java/guru/springframework/services/ConstructorGreetingService.java
+++ src/main/java/guru/springframework/services/ConstructorGreetingService.java
@@ -1,4 +1,4 @@
-package services;
+package guru.springframework.services;
 import org.springframework.stereotype.Service;
 
 @Service
--- src/main/java/guru/springframework/services/GreetingService.java
+++ src/main/java/guru/springframework/services/GreetingService.java
@@ -1,4 +1,4 @@
-package services;
+package guru.springframework.services;
 
 public interface GreetingService {
     String sayGreeting();
